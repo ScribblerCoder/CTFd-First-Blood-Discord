6236292c9af9137d1d084dd7e352c25c37ced1d5
ScribblerCoder <35840617+ScribblerCoder@users.noreply.github.com>
2023-04-30 23:58:02 +0300

    Add TIME_ZONE and IMG_URLS for the discord embed message

--- ctfd-first-blood-discord/config.py
+++ ctfd-first-blood-discord/config.py
@@ -5,7 +5,7 @@ HOST = "https://ctfd.io"
 API_TOKEN = ""
 
 # How frequently to check for new solves
-POLL_PERIOD = 10
+POLL_PERIOD = 5
 
 # Discord webhook url
 WEBHOOK_URL = ""
@@ -13,13 +13,13 @@ WEBHOOK_URL = ""
 # Available template variables are:
 # User Name (if individual mode) / Team Name (if team mode): {user_name}
 # Challenge Name: {chal_name}
-FIRST_BLOOD_ANNOUNCE_STRING = ":knife::drop_of_blood: First Blood for challenge **{chal_name}** goes to **{user_name}**! {emojis}"
+FIRST_BLOOD_ANNOUNCE_STRING = "```ini\n🩸 First Blood for challenge <{chal_name}> goes to team <{user_name}>!\n```"
 
 # To be used with announce_all_solves
-SOLVE_ANNOUNCE_STRING = "**{user_name}** just solved **{chal_name}**! {emojis}"
+SOLVE_ANNOUNCE_STRING = "```ini\n🚩 team <{user_name}> just solved [{chal_name}]!\n```"
 
 # Whether or not to announce all solves as well as first bloods
-ANNOUNCE_ALL_SOLVES = False
+ANNOUNCE_ALL_SOLVES = True
 
 # Category Emojis (if any)
 CATEGORY_EMOJIS = {
@@ -33,6 +33,8 @@ CATEGORY_EMOJIS = {
     "misc": [":jigsaw:"],
 }
 
+# GIFs and images to be added with the discord embed 
+IMG_URLS = ['https://miro.medium.com/fit/c/96/96/1*MptLjygMO184yyWqmg4PUA.gif','https://miro.medium.com/max/1400/1*fApaeq6-ht-s6Cdu6qdlrQ.gif','https://media.tenor.com/3bTxZ4HdrysAAAAC/pixels-neon.gif','https://64.media.tumblr.com/a4ceca22cc9c75d182f3f1bde1b154ea/1a81e7dc134d48c3-4e/s540x810/bbd495716a2f3624ad1e234ad254d639c38a6f51.gif','https://media.tenor.com/tm3KA5yrnmMAAAAC/hacker-man-hacker.gif','https://media.tenor.com/9Pn19IB5kYcAAAAC/hacking-computer-screen.gif','https://i.imgur.com/KvehwgO.png','https://media.tenor.com/yOwKX_hMp6cAAAAd/hackerman-rami-malek.gif','https://media.tenor.com/Pm4S40MGsIQAAAAC/hacker-hackerman.gif','https://i.giphy.com/media/o0vwzuFwCGAFO/giphy.webp']
 
-# Timezone for the time in the embed discord message
-TIME_ZONE = 'Asia/Amman'
+# Timezone of the competition to add current time of the firstblood in the discord embed
+TIME_ZONE = 'America/New_York'
